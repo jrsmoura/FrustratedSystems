21a2b8e7a1f6888905210ffdd71ae21e5933cc1b
jrsmoura <jrsteiner78@gmail.com>
2016-09-17 18:46:06 -0300

    Fase inicial de vidros de spin.

--- base/MonteCarloMove/MonteCarlo.py
+++ base/MonteCarloMove/MonteCarlo.py
@@ -17,16 +17,16 @@ def mcmove2d(config, beta, L):
         config[a, b] = s
     return config
 
-def mcmove1dSP(config, coup, beta, L):
+def mcmove1dSP(config, coup, beta, N):
     for i in range(L):
         a = np.random.randint(0, L)
-        b = np.random.randint(0, L)
-        s =  config[a, b]
-        nb = config[(a+1)%L, b] + config[(a-1)%L, b] + config[a, (b+1)%L] + config[a, (b-1)%L]
+        s =  config[a]
+        J = coup[a]
+        nb = config[(a+1)%L] + config[(a-1)%L]
         cost = 2*s*nb
         if cost < 0:
             s *= -1
-        elif rand() < np.exp(-cost*beta):
+        elif rand() < J*np.exp(-cost*beta):
             s *= -1
         config[a, b] = s
     return config
